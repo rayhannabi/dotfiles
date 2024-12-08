87ee4c16b541bde8ae87b0b40e71c38c91325b09
Rayhan Nabi <rayhan.nabi@monstar-lab.com>
2024-12-08 19:58:04 +0600

    use tokyonight with transparent bg + lazy update

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,18 @@
 return {
   {
-    "decaycs/decay.nvim",
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
   },
-
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = function()
-        local decay = require("decay")
-        local decay_core = require("decay.core")
-        local colors = decay_core.get_colors("default")
-        decay.setup({
-          override = {
-            WinSeparator = { fg = colors.black },
-          },
-        })
-        decay.load()
-      end,
+      colorscheme = "tokyonight",
     },
   },
 }
