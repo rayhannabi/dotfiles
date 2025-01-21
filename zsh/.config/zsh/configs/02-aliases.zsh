14dcd3be22517935029596c642b5e8d19520054d
Rayhan Nabi <rayhanjanam@gmail.com>
2025-01-21 14:03:19 +0600

    add zsh plugins as submodules

--- zsh/.config/zsh/configs/02-aliases.zsh
+++ zsh/.config/zsh/configs/02-aliases.zsh
@@ -3,8 +3,9 @@
 # Alias definitions
 
 alias zshrc="eval '$EDITOR $ZDOTDIR/.zshrc'"
-alias zshprofile="eval '$EDITOR $ZDOTDIR/.zprofile'"
 alias zshenv="eval '$EDITOR $HOME/.zshenv'"
+alias zshcomp-rebuild="rm $ZDOTDIR/.zcompdump; compinit; bashcompinit"
 
 alias ls=eza
+alias l="ls -alh --icons=always --colour=auto --sort=type"
 alias lg=lazygit
