ae353d6ca38891a6b32be175d02e01705ba94e07
Rayhan Nabi <rayhanjanam@gmail.com>
2025-01-21 17:15:47 +0600

    add fzf settings

--- zsh/.config/zsh/configs/11-fzf.zsh
+++ zsh/.config/zsh/configs/11-fzf.zsh
@@ -2,5 +2,29 @@
 #
 # FZF
 #
+FZF_COLORS="bg:-1,\
+bg+:-1,\
+fg:gray,\
+fg+:bright-white,\
+border:gray,\
+spinner:magenta,\
+hl:yellow,\
+header:blue,\
+info:green,\
+pointer:yellow,\
+marker:green,\
+prompt:blue,\
+hl+:red,\
+gutter:-1"
+
+export FZF_DEFAULT_OPTS="
+  --style=full \
+  --layout=reverse \
+  --prompt='󰍉 '\
+  --pointer=' ' \
+  --marker=' ' \
+	--color=$FZF_COLORS"
 
 source <(fzf --zsh)
+
+alias fzfp="fzf --preview='fzf-preview.sh {}'"
