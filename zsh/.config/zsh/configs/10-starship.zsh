10a3b5794173b7c884a46cab7481c6bc29538c1d
Rayhan Nabi <rayhanjanam@gmail.com>
2025-09-01 10:19:34 +0600

    add starship ascii config

--- zsh/.config/zsh/configs/10-starship.zsh
+++ zsh/.config/zsh/configs/10-starship.zsh
@@ -3,15 +3,22 @@
 # Starship
 #
 
-conf="$XDG_CONFIG_HOME/starship/config.toml"
-conf_plain="$XDG_CONFIG_HOME/starship/config-plaintext.toml"
+CONF="$XDG_CONFIG_HOME/starship/config.toml"
+CONF_ASCII="$XDG_CONFIG_HOME/starship/config-ascii.toml"
 
 if os_is_darwin; then
-  export STARSHIP_CONFIG=$conf
-else
+  export STARSHIP_CONFIG=$CONF
+fi
+
+if os_is_linux; then
   case $(tty) in
-  /dev/tty[0-9]*) export STARSHIP_CONFIG=$conf_plain ;;
-  *) export STARSHIP_CONFIG=$conf ;;
+  /dev/tty[0-9]*)
+    export STARSHIP_CONFIG=$CONF_ASCII
+    ;;
+  *)
+    export STARSHIP_CONFIG=$CONF_ASCII
+    ;;
   esac
 fi
+
 eval "$(starship init zsh)"
