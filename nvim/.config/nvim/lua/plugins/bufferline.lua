b0a9a58cdf3d6962ae38e9ddc83070a9ce9616c4
Rayhan Nabi <rayhanjanam@gmail.com>
2025-11-06 08:49:47 +0600

    feat: Add jetpack starship theme and update lualine config
    This commit introduces a new starship theme called "jetpack" and adds a configuration for the lualine plugin in Neovim.
    
    The bufferline separator style is also updated.

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -2,7 +2,6 @@ return {
   "akinsho/bufferline.nvim",
   opts = {
     options = {
-      separator_style = "slant",
       buffer_close_icon = "󰅖",
       close_icon = "󰅖",
     },
--- zsh/.config/zsh/configs/10-starship.zsh
+++ zsh/.config/zsh/configs/10-starship.zsh
@@ -5,9 +5,15 @@
 
 CONF="$XDG_CONFIG_HOME/starship/config.toml"
 CONF_ASCII="$XDG_CONFIG_HOME/starship/config-ascii.toml"
+CONF_JETPACK="$XDG_CONFIG_HOME/starship/config-jetpack.toml"
 
 if term_is_nice; then
-  export STARSHIP_CONFIG=$CONF
+  # check if $JETPACK is set to 1
+  if [[ -n "$JETPACK" && "$JETPACK" -eq 1 ]]; then
+    export STARSHIP_CONFIG=$CONF_JETPACK
+  else
+    export STARSHIP_CONFIG=$CONF
+  fi
 else
   export STARSHIP_CONFIG=$CONF_ASCII
 fi
