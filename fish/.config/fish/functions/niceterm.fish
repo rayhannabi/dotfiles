8bb25cdbe4750b3155afd1a02215abce0f8e2a2d
Rayhan Nabi <rayhanjanam@gmail.com>
2026-02-10 14:22:39 +0600

    add xterm-256color support for detection

--- fish/.config/fish/functions/niceterm.fish
+++ fish/.config/fish/functions/niceterm.fish
@@ -3,7 +3,7 @@
 function niceterm
     # Check if the terminal supports nice features
     set -l term_programs ghostty kitty iTerm.app vscode Apple_Terminal
-    set -l term_names xterm-ghostty xterm-kitty alacritty terminator konsole hyper wezterm
+    set -l term_names xterm-ghostty xterm-kitty alacritty terminator konsole hyper wezterm xterm-256color
 
     if contains $TERM_PROGRAM $term_programs || contains $TERM $term_names
         return 0
