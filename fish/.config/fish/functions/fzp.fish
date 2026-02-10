d27af029a62dc454e3419c49f21f70158868b726
Rayhan Nabi <rayhanjanam@gmail.com>
2026-02-10 15:01:41 +0600

    fix fzf preview on linux

--- fish/.config/fish/functions/fzp.fish
+++ fish/.config/fish/functions/fzp.fish
@@ -1,3 +1,8 @@
 function fzp --wraps='fzf --preview="fzf-preview.sh {}"' --description 'alias fzp fzf --preview="fzf-preview.sh {}"'
-    fzf --preview="fzf-preview.sh {}" $argv
+    if test (uname) = Linux
+        set preview '/usr/share/fzf/fzf-preview.sh {}'
+    else
+        set preview 'fzf-preview.sh {}'
+    end
+    fzf --preview=$preview $argv
 end
