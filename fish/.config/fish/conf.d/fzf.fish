728d81f78e5798339a70dd71f027fa3b1b038958
Rayhan Nabi <rayhanjanam@gmail.com>
2026-02-16 13:46:18 +0600

    add keybinds for fzf in fish

--- fish/.config/fish/conf.d/fzf.fish
+++ fish/.config/fish/conf.d/fzf.fish
@@ -2,6 +2,23 @@
 
 # FZF
 
+if niceterm
+    set prompt '󰍉 '
+    set pointer ' '
+    set marker '✓ '
+else
+    set prompt '? '
+    set pointer '> '
+    set marker '+ '
+end
+
+switch (uname)
+    case Darwin
+        set preview 'fzf-preview.sh'
+    case Linux
+        set preview '/usr/share/fzf/fzf-preview.sh'
+end
+
 set -l fzf_colors "bg:-1,
 bg+:-1,
 fg:white,
@@ -16,22 +33,20 @@ marker:green,
 prompt:blue,
 hl+:red"
 
-if niceterm
-    set prompt '󰍉 '
-    set pointer ' '
-    set marker '✓ '
-else
-    set prompt '? '
-    set pointer '> '
-    set marker '+ '
-end
+set -gx FZF_DEFAULT_OPTS "--style full
+--layout reverse
+--prompt '$prompt'
+--pointer '$pointer'
+--marker '$marker'
+--gutter ' '
+--color '$fzf_colors'
+--preview '$preview {}'
+--bind 'ctrl-e:become(nvim {+})'
+--bind 'ctrl-o:become(bat {+})'
+--bind 'ctrl-/:change-preview-window(hidden|)'"
 
-set -gx FZF_DEFAULT_OPTS "--style=full
---layout=reverse
---prompt='$prompt'
---pointer='$pointer'
---marker='$marker'
---gutter=' '
---color='$fzf_colors'"
+set -gx FZF_ALT_C_OPTS "
+--preview 'eza -T {}'
+--bind 'ctrl-/:change-preview-window(hidden|)'"
 
 fzf --fish | source
--- fish/.config/fish/functions/fzp.fish
+++
@@ -1,8 +0,0 @@
-function fzp --wraps='fzf --preview="fzf-preview.sh {}"' --description 'alias fzp fzf --preview="fzf-preview.sh {}"'
-    if test (uname) = Linux
-        set preview '/usr/share/fzf/fzf-preview.sh {}'
-    else
-        set preview 'fzf-preview.sh {}'
-    end
-    fzf --preview=$preview $argv
-end
